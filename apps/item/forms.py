ac55ea102706808a5bb1811e2238aec25bece2fe
lauro <lauropimentel12@gmail.com>
2022-07-20 22:55:04 -0300

    form get_comapany

--- apps/item/forms.py
+++ apps/item/forms.py
@@ -12,6 +12,7 @@ class ItemForm(CompanyAddonForm):
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
         self.service_order_id = kwargs.pop('service_order_id','')
+        self.get_company()
         super(ItemForm, self).__init__(*args, **kwargs)
         # self.fields['serviceorders'] = forms.ModelChoiceField(queryset=ServiceOrder.objects.filter(main_os=self.service_order_id, company=self.company))
 
--- apps/process/forms.py
+++ apps/process/forms.py
@@ -1,13 +1,14 @@
 
 from django import forms
 from .models import Process
+from ..forms_addons import CompanyAddonForm
 
-
-class ProcessForm(forms.ModelForm):
+class ProcessForm(CompanyAddonForm):
     model = Process
     
     def __init__(self, *args, **kwargs):
         self.request = kwargs.pop("request")
+        self.get_company()
         #self.service_order_id = kwargs.pop('service_order_id','')
         super(ProcessForm, self).__init__(*args, **kwargs)
         #self.fields['serviceorders'] = forms.ModelChoiceField(queryset=ServiceOrder.objects.filter(main_os=self.service_order_id, company=self.company))
